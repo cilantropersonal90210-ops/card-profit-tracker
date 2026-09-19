f1fd2ae40b61eaec031b2dbda96774ae9b275566
cilantropersonal90210-ops <cilantropersonal90210@gmail.com>
2026-09-19 00:27:15 -0400

    script.js

--- script.js
+++ script.js
@@ -76,6 +76,11 @@ cardElement.innerHTML = `
 
     <p>Expected Sale Price: $${Number(card.expectedSalePrice).toFixed(2)}</p>
 
+    <p>Potential Profit: $${(
+        Number(card.expectedSalePrice) -
+        Number(card.buyPrice)
+    ).toFixed(2)}</p>
+
     <p>
         Status:
         <span class="card-status ${
