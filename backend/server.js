ef863bc98291d8590df46d04c80bc92986f3f217
cilantropersonal90210-ops <cilantropersonal90210@gmail.com>
2026-09-24 15:59:03 -0400

    server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,20 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+
+    if (req.url === "/api/test") {
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        });
+
+        res.end(JSON.stringify({
+            success: true,
+            message: "Card Profit API is working!"
+        }));
+
+        return;
+    }
+
     res.writeHead(200, {
         "Content-Type": "text/plain"
     });
